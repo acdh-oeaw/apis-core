c26a215dfe6a4d64d0abd1e947a15156dcd4b3a9
Matthias Schlögl <m.schloegl@gmail.com>
2018-01-25 12:48:44 +0100

    added a sparql based implementation for additional autocompletes

--- entities/custom_autocompletes.py
+++ entities/custom_autocompletes.py
@@ -1,3 +1,43 @@
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+def sparql_coin_autocomplete(q, page_size=20, offset=0):
+
+    query = """
+            PREFIX bio:	<http://purl.org/vocab/bio/0.1/>
+            PREFIX crm:	<http://www.cidoc-crm.org/cidoc-crm/>
+            PREFIX dcmitype:	<http://purl.org/dc/dcmitype/>
+            PREFIX dcterms:	<http://purl.org/dc/terms/>
+            PREFIX foaf:	<http://xmlns.com/foaf/0.1/>
+            PREFIX geo:	<http://www.w3.org/2003/01/geo/wgs84_pos#>
+            PREFIX nm:	<http://nomisma.org/id/>
+            PREFIX nmo:	<http://nomisma.org/ontology#>
+            PREFIX org: <http://www.w3.org/ns/org#>
+            PREFIX osgeo:	<http://data.ordnancesurvey.co.uk/ontology/geometry/>
+            PREFIX rdac:	<http://www.rdaregistry.info/Elements/c/>
+            PREFIX skos:	<http://www.w3.org/2004/02/skos/core#>
+            PREFIX spatial: <http://jena.apache.org/spatial#>
+            PREFIX void:	<http://rdfs.org/ns/void#>
+            PREFIX xsd:	<http://www.w3.org/2001/XMLSchema#>
+
+            SELECT * WHERE {{
+              ?o a nmo:NumismaticObject .
+              ?o dcterms:title ?name .FILTER (contains(?name,"{}")) .
+              ?o dcterms:identifier ?id .
+              OPTIONAL {{?o foaf:thumbnail ?pic}} .
+              OPTIONAL {{?o nmo:hasReverse ?rev .
+                       ?rev foaf:depiction ?pic}} .
+            }} LIMIT {}
+            OFFSET {}""".format(q, page_size, offset*page_size)
+    sparql = SPARQLWrapper("http://nomisma.org/query")
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+    res = []
+    for r in results['results']['bindings']:
+        txt = '''<span class="autocomplete-span" data-pic="{}"><small>nom</small><b>{}</b></span>
+                '''.format(r['pic']['value'], r['name']['value'])
+        res.append({'text': txt, 'id': r['id']['value']})
+    return res
 
 
 class CustomEntityAutocompletes(object):
