6e2f43cdb0e9d5d94baeae75fd401d442f04a0b1
Matthias Schlögl <m.schloegl@gmail.com>
2018-03-08 13:39:01 +0100

    first version of detached highlighter app

--- relations/forms.py
+++ relations/forms.py
@@ -19,7 +19,6 @@ from vocabularies.models import (PersonInstitutionRelation, PersonPlaceRelation,
                                  PersonEventRelation, InstitutionEventRelation, PlaceEventRelation,
                                  InstitutionInstitutionRelation, PersonPersonRelation)
 from labels.models import Label
-from highlighter.models import Annotation
 from helper_functions.RDFparsers import GenericRDFParser
 from dal import autocomplete
 
@@ -107,86 +106,3 @@ class PlaceEntityForm(forms.Form):
         if not pl:
             pl = GenericRDFParser(cd['place_uri'], 'Place').get_or_create()
         return pl
-
-##############################################
-# Highlighter
-##############################################
-
-
-class PlaceEntityHighlighterForm(forms.Form):
-    place = forms.CharField(label='Place', widget=autocomplete.ModelSelect2(url='autocomplete/place'))
-    place_uri = forms.CharField(required=False, widget=forms.HiddenInput())
-    HL_start = forms.IntegerField(widget=forms.HiddenInput)
-    HL_end = forms.IntegerField(widget=forms.HiddenInput)
-    HL_text_id = forms.CharField(widget=forms.HiddenInput)
-
-    def save(self, *args, **kwargs):
-        cd = self.cleaned_data
-        pl = Place.get_or_create_uri(cd['place_uri'])
-        if not pl:
-            pl = GenericRDFParser(cd['place_uri'], 'Place').get_or_create()
-        txt = Text.objects.get(pk=cd['HL_text_id'][5:])
-        a = Annotation(
-            start=cd['HL_start'],
-            end=cd['HL_end'],
-            text=txt,
-            user_added=self.request.user,
-            annotation_project_id=int(self.request.session.get('annotation_project', 1)))
-        a.save()
-        a.entity_link.add(pl)
-        return a
-
-    def __init__(self, *args, **kwargs):
-        entity_type = kwargs.pop('entity_type', False)
-        self.request = kwargs.pop('request', False)
-        super(PlaceEntityHighlighterForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_class = 'PlaceEntityForm'
-        self.helper.form_tag = False
-
-
-
-
-class AddRelationHighlighterBaseForm(forms.Form):
-    HL_start = forms.IntegerField(widget=forms.HiddenInput)
-    HL_end = forms.IntegerField(widget=forms.HiddenInput)
-    HL_text_id = forms.CharField(widget=forms.HiddenInput)
-    RL_type = forms.CharField(widget=forms.HiddenInput)
-    RL_pk = forms.IntegerField(widget=forms.HiddenInput)
-
-    def save(self):
-        cd = self.cleaned_data
-        #x = super(AddRelationHighlighterBaseForm, self).save()
-        txt = Text.objects.get(pk=cd['HL_text_id'][5:])
-        ent = ContentType.objects.get(
-            pk=cd['RL_type']).model_class().objects.get(pk=cd['RL_pk'])
-        self.ann = Annotation(
-            start=cd['HL_start'],
-            end=cd['HL_end'],
-            text=txt,
-            user_added=self.request.user)
-        self.ann.save()
-        self.ann.entity_link.add(ent)
-        return self.ann
-
-
-class AddRelationHighlighterPersonForm(AddRelationHighlighterBaseForm):
-    # relation = forms.CharField(
-    #     label='Relation',
-    #     widget=al.TextWidget('AddRelationPersonHighlighterAutocomplete'),
-    #     )
-
-    def save(self, *args, **kwargs):
-        cd = self.cleaned_data
-        x = super().save()
-
-    def __init__(self, *args, **kwargs):
-        self.request = kwargs.pop('request', False)
-        self.entity_type = kwargs.pop('entity_type', False)
-        super(AddRelationHighlighterPersonForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_tag = False
-
-    def get_text_id(self):
-        return self.cleaned_data['HL_text_id'][5:]
-
