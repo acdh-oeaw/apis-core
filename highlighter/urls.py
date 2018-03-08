0b7fe5891dd9ce3313cd7a6ed3dc01a039e97143
Unknown <m.schloegl@gmail.com>
2018-03-08 13:54:15 +0100

    removing highlighter app

--- highlighter/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.conf import settings
-
-urlpatterns = []
-
-if 'annotator agreement' in getattr(settings, "APIS_COMPONENTS", []):
-    from highlighter.views import ComputeAgreement
-    urlpatterns.append(url(r'^agreement/$', ComputeAgreement.as_view(), name='agreement'))
