b288e3eb56baa44d70d346a603b2953005b2730a
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-11-10 11:36:52 +0100

    pep8 things

--- apis_core/apis_entities/serializers.py
+++ apis_core/apis_entities/serializers.py
@@ -10,24 +10,27 @@ class EntityUriSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     uri = serializers.URLField()
 
+
 class EntitySerializer(serializers.Serializer):
     id = serializers.IntegerField()
-    name = serializers.CharField() 
+    name = serializers.CharField()
     start_date = serializers.DateField()
     end_date = serializers.DateField()
     uris = EntityUriSerializer(source="uri_set", many=True)
-    
+
     def add_relations(self, obj):
         res = {}
         mk = obj.__class__.__name__
-        for rel in ContentType.objects.filter(app_label='apis_relations', model__icontains=mk.lower()):
+        for rel in ContentType.objects.filter(
+                app_label='apis_relations', model__icontains=mk.lower()):
             mk2 = re.match(r'{}([A-Za-z]+)'.format(mk.lower()), rel.model)
             if not mk2:
                 mk2 = re.match(r'([A-Za-z]+{})'.format(mk.lower()), rel.model)
             res['{}s'.format(mk2.group(1))] = []
             if mk2.group(1).lower() != mk.lower():
                 for rel2 in getattr(obj, '{}_set'.format(rel.model)).all():
-                    res['{}s'.format(mk2.group(1))].append(RelationEntitySerializer(rel2, own_class=mk, read_only=True).data)
+                    res['{}s'.format(mk2.group(1))].append(RelationEntitySerializer(
+                        rel2, own_class=mk, read_only=True).data)
             else:
                 for t in ['A', 'B']:
                     for rel2 in getattr(obj, 'related_{}{}'.format(mk.lower(), t)).all():
@@ -35,7 +38,9 @@ class EntitySerializer(serializers.Serializer):
                             ok = '{}A'.format(mk.lower())
                         else:
                             ok = '{}B'.format(mk.lower())
-                        res['{}s'.format(mk2.group(1))].append(RelationEntitySerializer(rel2, own_class=ok, read_only=True).data)
+                        res['{}s'.format(mk2.group(1))].append(
+                            RelationEntitySerializer(rel2, own_class=ok, read_only=True).data
+                        )
         return res
 
     def add_entity_type(self, obj):
@@ -43,14 +48,20 @@ class EntitySerializer(serializers.Serializer):
 
     def __init__(self, *args, depth_ent=1, **kwargs):
         super(EntitySerializer, self).__init__(*args, **kwargs)
-        
+
         for f in self.instance._meta.fields:
             field_name = re.search(r'([A-Za-z]+)\'>', str(f.__class__)).group(1)
-            if field_name in ['CharField', 'DateField', 'DateTimeField', 'IntegerField', 'FloatField']:
+            if field_name in [
+                'CharField', 'DateField', 'DateTimeField', 'IntegerField', 'FloatField'
+            ]:
                 self.fields[f.name] = getattr(serializers, field_name)()
-        self.fields['entity_type'] = serializers.SerializerMethodField(method_name="add_entity_type")
+        self.fields['entity_type'] = serializers.SerializerMethodField(
+            method_name="add_entity_type"
+        )
         if depth_ent == 1:
-            self.fields['relations'] = serializers.SerializerMethodField(method_name="add_relations")
+            self.fields['relations'] = serializers.SerializerMethodField(
+                method_name="add_relations"
+            )
 
 
 class RelationEntitySerializer(serializers.Serializer):
@@ -58,22 +69,26 @@ class RelationEntitySerializer(serializers.Serializer):
     relation_label = serializers.CharField(source="relation_type.label")
 
     def add_entity(self, obj):
-        return EntitySerializer(getattr(obj, "related_{}".format(self.entity_type)), depth_ent=0).data
-    
+        return EntitySerializer(
+            getattr(obj, "related_{}".format(self.entity_type)), depth_ent=0
+        ).data
+
     def __init__(self, *args, own_class=None, **kwargs):
         super(RelationEntitySerializer, self).__init__(*args, **kwargs)
         if self.instance is not None:
             for f in self.instance._meta.fields:
                 if f.name.startswith('related_'):
                     mk2 = f.name.replace('related_', '')
-                    
+
                     if mk2.lower() != own_class.lower():
                         self.entity_type = mk2
                         if re.match(r'.*[A-B]$', mk2):
                             mk2_ = mk2[:-1]
                         else:
                             mk2_ = mk2
-                        self.fields['{}'.format(mk2_)] = serializers.SerializerMethodField(method_name="add_entity")
+                        self.fields['{}'.format(mk2_)] = serializers.SerializerMethodField(
+                            method_name="add_entity"
+                        )
 
 
 class InstitutionSerializer(serializers.HyperlinkedModelSerializer):
@@ -111,13 +126,15 @@ class WorkSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('id', 'name', 'uri_set', 'collection', 'text')
 
 
-
 class GeoJsonSerializer(serializers.BaseSerializer):
 
     def to_representation(self, obj):
         p_pk = self.context.get('p_pk')
         short = False
-        url_r = reverse_lazy('apis:apis_entities:resolve_ambigue_place', kwargs={'pk': str(p_pk), 'uri': obj['id'][7:]})
+        url_r = reverse_lazy(
+            'apis:apis_entities:resolve_ambigue_place',
+            kwargs={'pk': str(p_pk), 'uri': obj['id'][7:]}
+        )
         long = False
         if 'http://www.w3.org/2003/01/geo/wgs84_pos#long' in obj.keys():
             long = float(obj['http://www.w3.org/2003/01/geo/wgs84_pos#long'][0]['value'])
@@ -180,8 +197,12 @@ class NetJsonNodeSerializer(serializers.BaseSerializer):
 
     def to_representation(self, obj):
         ent_obj = obj.__class__.__name__
-        ent_url = reverse_lazy('apis:apis_entities:generic_entities_edit_view', kwargs={'pk': str(obj.pk),
-                                                                              'entity': ent_obj.lower()})
+        ent_url = reverse_lazy(
+            'apis:apis_entities:generic_entities_edit_view',
+            kwargs={
+                'pk': str(obj.pk), 'entity': ent_obj.lower()
+                }
+            )
         tt = """<div class='arrow'></div>
             <div class='sigma-tooltip-header'>{}</div>
             <div class='sigma-tooltip-body'>
