0fdcb3daac2045db088b42ded6fdafd38b5c85b0
Matthias Schlögl <m.schloegl@gmail.com>
2018-12-10 18:53:02 +0100

    ichanges in the urls for the vis module

--- apis_core/apis_vis/urls.py
+++ apis_core/apis_vis/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from . import views
+from . import views, api_views
 
 app_name = 'apis_vis'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^heatmap-data/$', views.get_heatmap_data, name='get_heatmap_data'),
     url(r'^avg-age-data/$', views.get_average_age_data, name='get_avg_age_data'),
     url(r'^avg-age/$', views.AvgAge.as_view(), name='avgage_view'),
+    url(r'^person-institution-data/$', api_views.GetVisJson.as_view(), name='person-institution-data'),
 ]
