ad806c55c3222ff986f864bf1f3332935f9c8a27
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-12-10 13:50:54 +0000

    added average members view

--- apis_core/apis_vis/urls.py
+++ apis_core/apis_vis/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^heatmap-data/$', views.get_heatmap_data, name='get_heatmap_data'),
     url(r'^avg-age-data/$', views.get_average_age_data, name='get_avg_age_data'),
     url(r'^avg-age/$', views.AvgAge.as_view(), name='avgage_view'),
+    url(r'^avg-members-data/$', views.get_average_members_data, name='get_avg_members_data'),
+    url(r'^avg-members/$', views.MembersAmountPerYear.as_view(), name='avg_members_view'),
 ]
