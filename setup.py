9f6c969ee79419563f44a8dcafdba2f5b4dd5965
Matthias Schlögl <m.schloegl@gmail.com>
2019-01-06 22:00:58 +0100

    new pip version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ bower_json = {
 
 setup(
     name='apis-core',
-    version='0.9.12',
+    version='0.9.15',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',  # example license
