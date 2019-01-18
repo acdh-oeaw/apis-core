81c48be8662abb6481163975c42dc45b25f13214
Matthias Schlögl <m.schloegl@gmail.com>
2019-01-18 14:06:12 +0100

    added a function to refocus on fist elem of relation form after save #40

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ bower_json = {
 
 setup(
     name='apis-core',
-    version='0.9.18',
+    version='0.9.19',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',  # example license
