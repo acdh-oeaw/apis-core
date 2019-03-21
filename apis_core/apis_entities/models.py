85bc6acc79169ff47af2b1016e41920cdf8b0249
Matthias Schlögl <m.schloegl@gmail.com>
2019-03-21 15:42:56 +0100

    added tags for texts

--- apis_core/apis_entities/models.py
+++ apis_core/apis_entities/models.py
@@ -152,7 +152,8 @@ class Work(TempEntityClass):
     language = models.ForeignKey(
         WorkLanguage, blank=True, null=True, on_delete=models.SET_NULL
     )
-    denomiation = models.ManyToManyField(WorkDenomination, blank=True, null=True)
+    denomination = models.ManyToManyField(WorkDenomination, blank=True, null=True)
+    tag = models.ManyToManyField(WorkTag, blank=True, null=True)
 
     def __str__(self):
         if self.name != "":
