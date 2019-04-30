7089996a70dcb00aa628b26bfb3cc2369417c906
Matthias Schlögl <m.schloegl@gmail.com>
2019-04-30 14:51:45 +0200

    added urls and labels to objects

--- apis_core/api_routers.py
+++ apis_core/api_routers.py
@@ -4,6 +4,7 @@ from functools import reduce
 import django.db.models.fields as df_fields
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
+from django.urls import reverse
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, generics, routers, serializers, viewsets
 from rest_framework.permissions import AllowAny, DjangoObjectPermissions
@@ -15,6 +16,21 @@ def deep_get(dictionary, keys, default=None):
         keys.split("."),
         dictionary,
     )
+
+
+class LabelSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    label = serializers.SerializerMethodField(method_name="add_label")
+    uri = serializers.SerializerMethodField(method_name="add_uri")
+
+    def add_uri(self, obj):
+        return reverse(
+            "apis:apis_api:{}-detail".format(obj.__class__.__name__.lower()),
+            kwargs={"pk": obj.pk},
+        )
+
+    def add_label(self, obj):
+        return str(obj)
 
 
 def create_generic_api_viewset(**kwargs):
@@ -64,6 +80,9 @@ def create_generic_api_viewset(**kwargs):
                     view_name="apis:apis_api:{}-detail".format(entity_str.lower())
                 )
 
+                def add_labels(self, obj):
+                    return {"id": obj.pk, "label": str(obj)}
+
                 class Meta:
                     model = entity
                     exclude = exclude_lst
@@ -73,6 +92,18 @@ def create_generic_api_viewset(**kwargs):
                     for f in entity._meta.get_fields():
                         if f.name in exclude_lst:
                             continue
+                        elif (
+                            f.__class__.__name__ == "ManyToManyField"
+                            and f.name
+                            in deep_get(
+                                getattr(settings, app_label.upper(), {}),
+                                "{}.labels".format(entity_str),
+                                [],
+                            )
+                        ):
+                            self.fields[f.name] = LabelSerializer(
+                                many=True, read_only=True
+                            )
                         elif f.__class__.__name__ == "ManyToManyField":
                             self.fields[f.name] = serializers.HyperlinkedRelatedField(
                                 view_name="apis:apis_api:{}-detail".format(
@@ -81,6 +112,17 @@ def create_generic_api_viewset(**kwargs):
                                 read_only=True,
                                 many=True,
                             )
+                        elif (
+                            f.__class__.__name__ == "ForeignKey"
+                            and f.name
+                            in deep_get(
+                                getattr(settings, app_label.upper(), {}),
+                                "{}.labels".format(entity_str),
+                                [],
+                            )
+                        ):
+                            self.fields[f.name] = LabelSerializer(read_only=True)
+
                         elif f.__class__.__name__ == "ForeignKey":
                             self.fields[f.name] = serializers.HyperlinkedRelatedField(
                                 view_name="apis:apis_api:{}-detail".format(
@@ -88,14 +130,6 @@ def create_generic_api_viewset(**kwargs):
                                 ),
                                 read_only=True,
                             )
-                            for f in deep_get(
-                                getattr(settings, app_label.upper(), {}),
-                                "{}.labels".format(entity_str),
-                                [],
-                            ):
-                                self.fields[
-                                    "{}_label".format(f)
-                                ] = serializers.CharField(source=f)
 
             return CustomSerializer
 
