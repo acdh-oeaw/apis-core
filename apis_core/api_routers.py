2e93047ec172157f02edeb3a858ce0f875cb49f7
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2019-09-25 11:50:58 +0200

    added cache control method decorator for generic API towards fixing #115 cache-time to be set by MAX_AGE settings param; similar thing maybe to do for custom-entity endpoints and autocompletes

--- apis_core/api_routers.py
+++ apis_core/api_routers.py
@@ -5,6 +5,10 @@ import django.db.models.fields as df_fields
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.urls import reverse
+
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_control
+
 from url_filter.integrations.drf import DjangoFilterBackend
 from rest_framework import filters, generics, pagination, routers, serializers, viewsets
 from rest_framework.permissions import AllowAny, DjangoObjectPermissions
@@ -12,6 +16,12 @@ from rest_framework.response import Response
 from rest_framework import renderers
 from .api_renderers import NetJsonRenderer
 from url_filter.filtersets import ModelFilterSet
+
+
+try:
+    MAX_AGE = settings.MAX_AGE
+except AttributeError:
+    MAX_AGE = 0
 
 
 def deep_get(dictionary, keys, default=None):
@@ -55,9 +65,8 @@ def create_generic_api_viewset(**kwargs):
 
     class GenericFilterSet(ModelFilterSet):
         class Meta(object):
-            model = ContentType.objects.get(app_label=kwargs.get("app_label"), model=kwargs.get("entity")).model_class() 
- 
-    
+            model = ContentType.objects.get(app_label=kwargs.get("app_label"), model=kwargs.get("entity")).model_class()
+
     class GenericAPIViewSet(viewsets.ModelViewSet):
         pagination_class = CustomPagination
         entity_str = kwargs.get("entity")
@@ -72,6 +81,11 @@ def create_generic_api_viewset(**kwargs):
         test_search = getattr(settings, app_label.upper(), False)
         renderer_classes = (renderers.JSONRenderer, renderers.BrowsableAPIRenderer, NetJsonRenderer)
         filter_class = GenericFilterSet
+
+        @method_decorator(cache_control(max_age=MAX_AGE))
+        def dispatch(self, request, *args, **kwargs):
+            return super(GenericAPIViewSet, self).dispatch(request, *args, **kwargs)
+
         def get_serializer_class(self):
             entity_str = self.entity_str
             entity = self.entity
