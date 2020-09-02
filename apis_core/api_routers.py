baaa40f4024f10279c757faf5ec82de41c389273
steffres <stefan.resch@oeaw.ac.at>
2020-09-02 09:14:15 +0200

    helper for debugging production

--- apis_core/api_routers.py
+++ apis_core/api_routers.py
@@ -164,165 +164,166 @@ def generic_serializer_creation_factory():
 
 
         print(cont)
+        if cont != "passagelanguage":
 
-        test_search = getattr(settings, cont.app_label.upper(), False)
-        entity_str = str(cont).replace(' ', '')
-        entity = cont.model_class()
-        app_label = cont.app_label.replace(' ', '_')
-        exclude_lst = []
-        if app_label == "apis_entities":
-            exclude_lst = deep_get(
-                test_search, "{}.api_exclude".format(entity_str), []
-            )
-        else:
-            set_prem = getattr(settings, f"{cont.app_label.upper()}", {})
-            exclude_lst = deep_get(set_prem, "exclude", [])
-            exclude_lst.extend(
-                deep_get(set_prem, "{}.exclude".format(entity_str), [])
-            )
-        exclude_lst_fin = [x for x in exclude_lst if x in [x.name for x in entity._meta.get_fields()]]
-
-        class Meta:
-            model = entity
-            exclude = exclude_lst_fin
-
-        def to_representation_txt(self, instance):
-            res = {
-                'id': instance.pk,
-                'url': self.context['view'].request.build_absolute_uri(reverse(
-                        "apis:apis_api:{}-detail".format(instance.__class__.__name__.lower()),
-                        kwargs={"pk": instance.pk},
-                        )),
-                'text': instance.text
-            }
-            if self._highlight:
-                txt_html, annotations = highlight_text_new(instance,
-                    set_ann_proj=self._ann_proj_pk, types=self._types, users_show=self._users_show,
-                    inline_annotations=self._inline_annotations)
-                res['text'] = txt_html
-                res['annotations'] = annotations
-            return res
-
-        def init_text_serializer(self, *args, **kwargs):
-            super(self.__class__, self).__init__(*args, **kwargs)
-            action = self.context['view'].action
-            highlight = self.context['request'].query_params.get('highlight', None)
-            if action == 'retrieve' and highlight is not None and 'apis_highlighter' in getattr(settings,'INSTALLED_APPS'):
-                self._highlight = True
-                self._ann_proj_pk = self.context['request'].query_params.get('ann_proj_pk', None)
-                self._types = self.context['request'].query_params.get('types', None)
-                self._users_show = self.context['request'].query_params.get('users_show', None)
-                self._inline_annotations = self.context['request'].query_params.get('inline_annotations', True)
-                if self._inline_annotations.lower() == 'false':
-                    self._inline_annotations = False
-            else:
-                self._highlight = False
-
-
-        def init_serializers(self, *args, **kwargs):
-            super(self.__class__, self).__init__(*args, **kwargs)
-            entity_str = self._entity.__name__
-            action = self.context['view'].action
-            include = self.context['request'].query_params.get('include', None)
-            if include is not None and action == 'retrieve':
-                include = include.split(',')
-                if 'relations' in include:
-                    include = list(ContentType.objects.filter(app_label="apis_relations", model__icontains=entity_str).values_list('model', flat=True))
-                    print(include)
-                if len(include) > 0:
-                    self.fields['relations'] = RelationObjectSerializer(read_only=True, source='*', include=include)
-            app_label = self._app_label
-            lst_labels_set = deep_get(
-                    getattr(settings, app_label.upper(), {}),
-                    "{}.labels".format(entity_str),
-                    [],
+            test_search = getattr(settings, cont.app_label.upper(), False)
+            entity_str = str(cont).replace(' ', '')
+            entity = cont.model_class()
+            app_label = cont.app_label.replace(' ', '_')
+            exclude_lst = []
+            if app_label == "apis_entities":
+                exclude_lst = deep_get(
+                    test_search, "{}.api_exclude".format(entity_str), []
                 )
-            for f in self._entity._meta.get_fields():
-                if getattr(settings, "APIS_API_EXCLUDE_SETS", False) and str(f.name).endswith('_set'):
-                    if f.name in self.fields.keys():
-                        self.fields.pop(f.name)
-                    continue
-                ck_many = f.__class__.__name__ == 'ManyToManyField'
-                if f.name in self._exclude_lst:
-                    continue
-                elif f.__class__.__name__ in ["ManyToManyField", "ForeignKey"] and "apis_vocabularies" not in str(f.related_model):
-                    self.fields[f.name] = RelatedObjectSerializer(many=ck_many, read_only=True)
-                elif f.__class__.__name__ in ["ManyToManyField", "ForeignKey"]:
-                    self.fields[f.name] = LabelSerializer(many=ck_many, read_only=True)
-
-        s_dict = {
-            "id": serializers.ReadOnlyField(),
-            "url": serializers.HyperlinkedIdentityField(
-                view_name=f"apis:apis_api:{entity_str.lower()}-detail"),
-            "_entity": entity,
-            "_exclude_lst": exclude_lst_fin,
-            "_app_label": app_label,
-            "Meta": Meta,
-            "add_labels": lambda self, obj: {"id": obj.pk, "label": str(obj)},
-            "__init__": init_serializers
-        }
-        if entity_str.lower() == 'text':
-            s_dict['__init__'] = init_text_serializer
-            s_dict['to_representation'] = to_representation_txt
-        serializer_class = type(f"{entity_str.title().replace(' ', '')}Serializer", (serializers.HyperlinkedModelSerializer,), s_dict)
-        allowed_fields_filter = {'IntegerField': ['in', 'range'],
-                                 'CharField': ['exact', 'icontains', 'iregex', 'isnull'],
-                                 'BooleanField': ['exact'],
-                                 'DateField': ['year', 'lte', 'gte', 'day', 'month']}
-        filterset_dict = {}
-        filter_fields = []
-
-        for field in entity._meta.fields:
-            if field.__class__.__name__ == 'AutoField':
-                f_class = 'IntegerField'
             else:
-                f_class = field.__class__.__name__
-            if f_class not in allowed_fields_filter.keys():
-                continue
-            filterset_dict[field.name] = Filter(form_field=getattr(forms, f_class)(),
-                                                lookups=allowed_fields_filter[f_class])
-            filter_fields.append(field.name)
+                set_prem = getattr(settings, f"{cont.app_label.upper()}", {})
+                exclude_lst = deep_get(set_prem, "exclude", [])
+                exclude_lst.extend(
+                    deep_get(set_prem, "{}.exclude".format(entity_str), [])
+                )
+            exclude_lst_fin = [x for x in exclude_lst if x in [x.name for x in entity._meta.get_fields()]]
 
-        class MetaFilter(object):
-            model = entity
-            fields = filter_fields
+            class Meta:
+                model = entity
+                exclude = exclude_lst_fin
 
-        filterset_dict['Meta'] = MetaFilter
+            def to_representation_txt(self, instance):
+                res = {
+                    'id': instance.pk,
+                    'url': self.context['view'].request.build_absolute_uri(reverse(
+                            "apis:apis_api:{}-detail".format(instance.__class__.__name__.lower()),
+                            kwargs={"pk": instance.pk},
+                            )),
+                    'text': instance.text
+                }
+                if self._highlight:
+                    txt_html, annotations = highlight_text_new(instance,
+                        set_ann_proj=self._ann_proj_pk, types=self._types, users_show=self._users_show,
+                        inline_annotations=self._inline_annotations)
+                    res['text'] = txt_html
+                    res['annotations'] = annotations
+                return res
 
-        filter_class = type(f"Generic{entity_str.title().replace(' ', '')}FilterClass", (ModelFilterSet,), filterset_dict)
+            def init_text_serializer(self, *args, **kwargs):
+                super(self.__class__, self).__init__(*args, **kwargs)
+                action = self.context['view'].action
+                highlight = self.context['request'].query_params.get('highlight', None)
+                if action == 'retrieve' and highlight is not None and 'apis_highlighter' in getattr(settings,'INSTALLED_APPS'):
+                    self._highlight = True
+                    self._ann_proj_pk = self.context['request'].query_params.get('ann_proj_pk', None)
+                    self._types = self.context['request'].query_params.get('types', None)
+                    self._users_show = self.context['request'].query_params.get('users_show', None)
+                    self._inline_annotations = self.context['request'].query_params.get('inline_annotations', True)
+                    if self._inline_annotations.lower() == 'false':
+                        self._inline_annotations = False
+                else:
+                    self._highlight = False
 
-        def get_queryset(self):
-            if "apis_relations" in str(self.model):
-                return self.model.objects.filter_for_user()
-            return self.model.objects.all()
 
-        @swagger_auto_schema(filter_inspectors=[DjangoFilterDescriptionInspector,])
-        def list_viewset(self, request):
-            res = super(self.__class__, self).list(request)
-            return res
+            def init_serializers(self, *args, **kwargs):
+                super(self.__class__, self).__init__(*args, **kwargs)
+                entity_str = self._entity.__name__
+                action = self.context['view'].action
+                include = self.context['request'].query_params.get('include', None)
+                if include is not None and action == 'retrieve':
+                    include = include.split(',')
+                    if 'relations' in include:
+                        include = list(ContentType.objects.filter(app_label="apis_relations", model__icontains=entity_str).values_list('model', flat=True))
+                        print(include)
+                    if len(include) > 0:
+                        self.fields['relations'] = RelationObjectSerializer(read_only=True, source='*', include=include)
+                app_label = self._app_label
+                lst_labels_set = deep_get(
+                        getattr(settings, app_label.upper(), {}),
+                        "{}.labels".format(entity_str),
+                        [],
+                    )
+                for f in self._entity._meta.get_fields():
+                    if getattr(settings, "APIS_API_EXCLUDE_SETS", False) and str(f.name).endswith('_set'):
+                        if f.name in self.fields.keys():
+                            self.fields.pop(f.name)
+                        continue
+                    ck_many = f.__class__.__name__ == 'ManyToManyField'
+                    if f.name in self._exclude_lst:
+                        continue
+                    elif f.__class__.__name__ in ["ManyToManyField", "ForeignKey"] and "apis_vocabularies" not in str(f.related_model):
+                        self.fields[f.name] = RelatedObjectSerializer(many=ck_many, read_only=True)
+                    elif f.__class__.__name__ in ["ManyToManyField", "ForeignKey"]:
+                        self.fields[f.name] = LabelSerializer(many=ck_many, read_only=True)
 
-        @swagger_auto_schema(manual_parameters=lst_parameters_retrieve_txt)
-        def retrieve_view_txt(self, request, pk=None):
-            res = super(self.__class__, self).retrieve(request, pk=pk)
-            return res
-
-        viewset_dict = {
-            'pagination_class': CustomPagination,
-            'model': entity,
-            #'queryset': entity.objects.all(),
-            'filter_backends': (DjangoFilterBackend, ),
-            #'filter_fields': ['']
-            'depth': 2,
-            'renderer_classes': (renderers.JSONRenderer, renderers.BrowsableAPIRenderer, NetJsonRenderer),
-            'filter_class': filter_class,
-            'serializer_class': serializer_class,
-            "get_queryset": get_queryset,
-            #'list': list_viewset,
-            'dispatch': lambda self, request, *args, **kwargs: super(self.__class__, self).dispatch(request, *args, **kwargs)
+            s_dict = {
+                "id": serializers.ReadOnlyField(),
+                "url": serializers.HyperlinkedIdentityField(
+                    view_name=f"apis:apis_api:{entity_str.lower()}-detail"),
+                "_entity": entity,
+                "_exclude_lst": exclude_lst_fin,
+                "_app_label": app_label,
+                "Meta": Meta,
+                "add_labels": lambda self, obj: {"id": obj.pk, "label": str(obj)},
+                "__init__": init_serializers
             }
-        if entity_str.lower() == 'text':
-            viewset_dict['retrieve'] = retrieve_view_txt
-        views[f"{entity_str.lower().replace(' ', '')}"] = type(f"Generic{entity_str.title().replace(' ', '')}ViewSet", (viewsets.ModelViewSet, ), viewset_dict)
+            if entity_str.lower() == 'text':
+                s_dict['__init__'] = init_text_serializer
+                s_dict['to_representation'] = to_representation_txt
+            serializer_class = type(f"{entity_str.title().replace(' ', '')}Serializer", (serializers.HyperlinkedModelSerializer,), s_dict)
+            allowed_fields_filter = {'IntegerField': ['in', 'range'],
+                                     'CharField': ['exact', 'icontains', 'iregex', 'isnull'],
+                                     'BooleanField': ['exact'],
+                                     'DateField': ['year', 'lte', 'gte', 'day', 'month']}
+            filterset_dict = {}
+            filter_fields = []
+
+            for field in entity._meta.fields:
+                if field.__class__.__name__ == 'AutoField':
+                    f_class = 'IntegerField'
+                else:
+                    f_class = field.__class__.__name__
+                if f_class not in allowed_fields_filter.keys():
+                    continue
+                filterset_dict[field.name] = Filter(form_field=getattr(forms, f_class)(),
+                                                    lookups=allowed_fields_filter[f_class])
+                filter_fields.append(field.name)
+    
+            class MetaFilter(object):
+                model = entity
+                fields = filter_fields
+
+            filterset_dict['Meta'] = MetaFilter
+
+            filter_class = type(f"Generic{entity_str.title().replace(' ', '')}FilterClass", (ModelFilterSet,), filterset_dict)
+
+            def get_queryset(self):
+                if "apis_relations" in str(self.model):
+                    return self.model.objects.filter_for_user()
+                return self.model.objects.all()
+
+            @swagger_auto_schema(filter_inspectors=[DjangoFilterDescriptionInspector,])
+            def list_viewset(self, request):
+                res = super(self.__class__, self).list(request)
+                return res
+
+            @swagger_auto_schema(manual_parameters=lst_parameters_retrieve_txt)
+            def retrieve_view_txt(self, request, pk=None):
+                res = super(self.__class__, self).retrieve(request, pk=pk)
+                return res
+
+            viewset_dict = {
+                'pagination_class': CustomPagination,
+                'model': entity,
+                #'queryset': entity.objects.all(),
+                'filter_backends': (DjangoFilterBackend, ),
+                #'filter_fields': ['']
+                'depth': 2,
+                'renderer_classes': (renderers.JSONRenderer, renderers.BrowsableAPIRenderer, NetJsonRenderer),
+                'filter_class': filter_class,
+                'serializer_class': serializer_class,
+                "get_queryset": get_queryset,
+                #'list': list_viewset,
+                'dispatch': lambda self, request, *args, **kwargs: super(self.__class__, self).dispatch(request, *args, **kwargs)
+                }
+            if entity_str.lower() == 'text':
+                viewset_dict['retrieve'] = retrieve_view_txt
+            views[f"{entity_str.lower().replace(' ', '')}"] = type(f"Generic{entity_str.title().replace(' ', '')}ViewSet", (viewsets.ModelViewSet, ), viewset_dict)
 
 
 views = dict()
