9a49a38b03cd34a7a7852d039a12e14b66df61cc
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2021-10-18 17:30:54 +0000

    fixed an error in cidoc serialization

--- apis_core/apis_entities/api_mappings/cidoc_mapping.py
+++ apis_core/apis_entities/api_mappings/cidoc_mapping.py
@@ -107,7 +107,7 @@ def m_place_of_death(g, p, ns, data):
     return g
 
 
-def m_institutions_rel(g, p, ns, data):
+def m_institutions_rel(g, p, ns, data, drill_down=False):
     b_joining = URIRef(f"{base_uri}/events/joined/{data['id']}")
     g.add((b_joining, RDF.type, ns["cidoc"].E85_Joining))
     g.add((b_joining, ns["cidoc"].P143_joined, p))
