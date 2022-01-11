a5e537853b5422f70fb4f9c25f1f409edd132105
Matthias Schlögl <matthias.schloegl@oeaw.ac.at>
2022-01-11 13:47:55 +0100

    checkanbump did not recognize greater versions set in pyproject file

--- checkandbump.sh
+++ checkandbump.sh
@@ -2,13 +2,14 @@
 # Shell script to check the apis-core version on pypi and bump it
 
 CURRENTLY_BUILT_VER=$(sed -n '3s/.*= "\(.*\)"/\1/p' pyproject.toml)
-LASTVER=$(lastversion apis-core --at pip -gt ${CURRENTLY_BUILT_VER})
-echo $LASTVER
+LASTVER_PIP=$(lastversion apis-core --at pip -gt ${CURRENTLY_BUILT_VER})
+echo $LASTVER_PIP
 echo $CURRENTLY_BUILT_VER
-if [[ $? -eq 0 ]]; then
+LASTVER=$(lastversion ${CURRENTLY_BUILT_VER} -gt ${LASTVER_PIP})
+echo $LASTVER
+if [ "$CURRENTLY_BUILT_VER" == "$LASTVER" ]; then
+    echo "version already newer"; else
     echo "patching version"
-    LASTVER_PYPI=$(lastversion apis-core --at pip)
-    sed -i "3s/\".*$/\"${LASTVER_PYPI}\"/" pyproject.toml
-    poetry version patch; else
-    echo "version already newer"
+    sed -i "3s/\".*$/\"${LASTVER_PIP}\"/" pyproject.toml
+    poetry version patch
 fi
