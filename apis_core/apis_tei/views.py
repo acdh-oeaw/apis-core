428c0ec3b809f062518154b72958edca9c59adba
Gregor Pirgie <78487560+gregorpirgie@users.noreply.github.com>
2023-04-25 16:46:01 +0200

    fix: fixes 392; - keeps implementation with get_object_from_pk_or_uri - adds isinstance check for each endpoint - redirects with error message in plain-text if instance check fails.

--- apis_core/apis_tei/views.py
+++ apis_core/apis_tei/views.py
@@ -4,12 +4,18 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect
 
 from .tei_utils import get_node_from_template
+
 from apis_core.apis_metainfo.models import Uri
-from apis_core.apis_entities.models import Person, Institution, Place, Work
+from apis_core.apis_entities.models import Person, Place, Institution, Work
+from apis_core.apis_entities.detail_views import get_object_from_pk_or_uri
+
 
 def person_as_tei(request, pk):
     full = request.GET.get('full')
-    res = get_object_or_404(Person, pk=pk)
+    model = Person
+    res = get_object_from_pk_or_uri(request, pk)
+    if not isinstance(res, model):
+        return HttpResponse(f"Requested object is not an instance of {model.__name__}", content_type="text/plain")
     doc = get_node_from_template('apis_tei/person.xml', res, full=full)
     tei = ET.tostring(doc, pretty_print=True, encoding='UTF-8')
     return HttpResponse(tei, content_type="application/xml")
@@ -17,7 +23,10 @@ def person_as_tei(request, pk):
 
 def place_as_tei(request, pk):
     full = request.GET.get('full')
-    res = get_object_or_404(Place, pk=pk)
+    model = Place
+    res = get_object_from_pk_or_uri(request, pk)
+    if not isinstance(res, model):
+        return HttpResponse(f"Requested object is not an instance of {model.__name__}", content_type="text/plain")
     doc = get_node_from_template('apis_tei/place.xml', res, full=full)
     tei = ET.tostring(doc, pretty_print=True, encoding='UTF-8')
     return HttpResponse(tei, content_type="application/xml")
@@ -25,7 +34,10 @@ def place_as_tei(request, pk):
 
 def work_as_tei(request, pk):
     full = request.GET.get('full')
-    res = get_object_or_404(Work, pk=pk)
+    model = Work
+    res = get_object_from_pk_or_uri(request, pk)
+    if not isinstance(res, model):
+        return HttpResponse(f"Requested object is not an instance of {model.__name__}", content_type="text/plain")
     doc = get_node_from_template('apis_tei/work.xml', res, full=full)
     tei = ET.tostring(doc, pretty_print=True, encoding='UTF-8')
     return HttpResponse(tei, content_type="application/xml")
@@ -33,7 +45,10 @@ def work_as_tei(request, pk):
 
 def org_as_tei(request, pk):
     full = request.GET.get('full')
-    res = get_object_or_404(Institution, pk=pk)
+    model = Institution
+    res = get_object_from_pk_or_uri(request, pk)
+    if not isinstance(res, model):
+        return HttpResponse(f"Requested object is not an instance of {model.__name__}", content_type="text/plain")
     doc = get_node_from_template('apis_tei/org.xml', res, full=full)
     tei = ET.tostring(doc, pretty_print=True, encoding='UTF-8')
     return HttpResponse(tei, content_type="application/xml")
